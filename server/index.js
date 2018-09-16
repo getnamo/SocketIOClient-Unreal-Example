c9e6eda1d76c659593a86f8542fbd8fec41f5e3a
getnamo <getnamo@gmail.com>
2018-09-16 12:18:03 -0700

    comments and slight modernization

--- server/index.js
+++ server/index.js
@@ -1,36 +1,40 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var util = require('util');
-var clients = [];
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const util = require('util');
+const port = 3000;
+const clients = [];
 
+//Server Web Client
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//When a client connects, bind each desired event to the client socket
 io.on('connection', function(socket){
 	//track connected clients via log
 	clients.push(socket.id);
-	var clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+	let clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
 	io.emit('chat message', clientConnectedMsg);
 	console.log(clientConnectedMsg);
 
 	//track disconnected clients via log
 	socket.on('disconnect', function(){
 		clients.pop(socket.id);
-		var clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+		let clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
 		io.emit('chat message', clientDisconnectedMsg);
 		console.log(clientDisconnectedMsg);
 	})
 
 	//multicast received message from client
 	socket.on('chat message', function(msg){
-		var combinedMsg = socket.id.substring(0,4) + ': ' + msg;
+		let combinedMsg = socket.id.substring(0,4) + ': ' + msg;
 		io.emit('chat message', combinedMsg);
 		console.log('multicast: ' + combinedMsg);
 	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//Start the Server
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
