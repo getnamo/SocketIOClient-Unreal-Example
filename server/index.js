4813c1269ba681375694be9fd0ee0ba89f2bbdea
getnamo <getnamo@gmail.com>
2020-04-13 13:08:22 -0700

    Update dependencies and modernize example a bit
    - refactor for on generic event
    - camel case  events "chat message" -> "chatMessage"
    - define event once and reference everywhere for both client and server
    - audix fix

--- server/index.js
+++ server/index.js
@@ -3,38 +3,41 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const util = require('util');
 const port = 3000;
-const clients = [];
+const clients = [];	//track connected clients
 
 //Server Web Client
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//make one reference to event name so it can be easily renamed 
+const chatEvent = "chatMessage";
+
 //When a client connects, bind each desired event to the client socket
-io.on('connection', function(socket){
+io.on('connection', socket =>{
 	//track connected clients via log
 	clients.push(socket.id);
-	let clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
-	io.emit('chat message', clientConnectedMsg);
+	const clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+	io.emit(chatEvent, clientConnectedMsg);
 	console.log(clientConnectedMsg);
 
 	//track disconnected clients via log
-	socket.on('disconnect', function(){
+	socket.on('disconnect', ()=>{
 		clients.pop(socket.id);
-		let clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
-		io.emit('chat message', clientDisconnectedMsg);
+		const clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+		io.emit(chatEvent, clientDisconnectedMsg);
 		console.log(clientDisconnectedMsg);
 	})
 
 	//multicast received message from client
-	socket.on('chat message', function(msg){
-		let combinedMsg = socket.id.substring(0,4) + ': ' + msg;
-		io.emit('chat message', combinedMsg);
+	socket.on(chatEvent, msg =>{
+		const combinedMsg = socket.id.substring(0,4) + ': ' + msg;
+		io.emit(chatEvent, combinedMsg);
 		console.log('multicast: ' + combinedMsg);
 	});
 });
 
 //Start the Server
-http.listen(port, function(){
+http.listen(port, () => {
   console.log('listening on *:' + port);
 });
